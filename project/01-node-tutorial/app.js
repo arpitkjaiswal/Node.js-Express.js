df57e149185542e2b2acecb200d31bfd9d770a6c
Arpit Kumar Jaiswal <arpit1.mitmpl2024@learner.manipal.edu>
2026-07-03 17:32:35 +0000

    Updated

--- project/01-node-tutorial/app.js
+++ project/01-node-tutorial/app.js
@@ -2,8 +2,10 @@
 const names = require('./4-names')
 const sayHi = require('./5-utlis')
 const data = require('./6-alternative-flavor')
-console.log(data)
 
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+
+require('./7-mind-grenade')
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
