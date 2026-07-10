333e2b08bc662aaafdcae600579dc195c4c8dc40
Arpit Kumar Jaiswal <arpit1.mitmpl2024@learner.manipal.edu>
2026-07-10 19:09:19 +0000

    Server

--- SERVER/index.js
+++ SERVER/index.js
@@ -2,22 +2,31 @@ const http = require('http');
 
 const fs = require("fs")
 
+const url = require('url')
+
+
 const myServer =  http.createServer( (req,res) => {
- const log = `${new Date()}:  ${req.url } New Req Recived\n`;
+    if(req.url === '/favicon.ico') return res.end()
+    const log = `${new Date()}:  ${req.url } New Req Recived\n`;
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl)
+ 
  fs.appendFile("log.txt", log, (err, data) => {
-   switch(req.url){
+   switch(myUrl.pathname){
     case '/' : res.end("HomePage");
     break
-    case '/about' : res.end("I am Arpit Jaiswal")
+    case '/about' : 
+    const username = myUrl.query.myname;
+  
+    res.end(`Hi,  ${username}`)
+
     break
     default:
         res.end("404  not found")
 
-   }
+    }
 
- })
-   
-  
+    })
 });
 
 
