139db2544a58de39b8c47e36a1a5c645ede51be4
Arpit Kumar Jaiswal <arpit1.mitmpl2024@learner.manipal.edu>
2026-07-11 19:56:16 +0000

    Rest API's

--- Project3/index.js
+++ Project3/index.js
@@ -12,9 +12,34 @@ app.get("/api/users", (req , res) => {
 });
 
 app.get("/users", (req,res) => {
-    return res.json(users);
+   const html = `
+   <ul>
+      ${users.map((user) =>`<li>${user.first_name}</li>` ).join("")}
+   </ul>
+   `
+   return res.send(html);
 })
 
+app.route("/api/users/:id")
+.get((req, res) => {
+
+const id = Number(req.params.id);
+const user  = users.find((user) => user.id === id);
+return res.json(user)
+
+})
+.patch((req,res) => {
+   // it will update the user with the given id
+    return res.json(user)
+})
+.delete((req,res) => {
+    // it will delete the user with the given id
+    return res.json(user)
+})
+
+app.post("/api/users", (req,res) => {
+    return res.json({message : "User Created"})
+})
 
 
 app.listen(Port , () => console.log(` Server started at PORT : ${Port} `))
